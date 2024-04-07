fde99bb05adcf5bb59886617f3f408c01e2012aa
Jhonatan Muñoz <jhonatanm255@hotmail.com>
2024-04-07 16:03:00 -0600

    se agrego la funcion para mostrar las tareas y tambien se agregaron iconos de check, edit y delete, faltaria agregarles las funcionalidades en un proximo commit

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
+    const botonEnviar = document.getElementById('btn');
+    const contenedorTareas = document.getElementById('tareas');
+    const inputNuevaTarea = document.getElementById('nueva-tarea');
 
+    botonEnviar.addEventListener('click', (e) => {
+        e.preventDefault();
+        agregarTarea();
+    });
 
-const botonEnviar = document.getElementById('btn');
-const contenedorTareas = document.getElementById('tareas');
+    inputNuevaTarea.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            agregarTarea();
+        };
+    });
 
-botonEnviar.addEventListener('click', (e) => {
-    e.preventDefault();
+    function agregarTarea() {
+        const nuevaTareaTexto = inputNuevaTarea.value.trim();
 
-    const nuevaTareaTexto = document.getElementById('nueva-tarea').value;
+        if (nuevaTareaTexto !== '') {
+            const nuevaTareaElemento = document.createElement('div');
+            nuevaTareaElemento.classList.add('tarea');
 
-    if (nuevaTareaTexto.trim() !== '') {
-        const nuevaTareaElemento = document.createElement('div');
-        nuevaTareaElemento.classList.add('tarea');
-        
-        nuevaTareaElemento.innerHTML = `
-            <p>${nuevaTareaTexto}</p>
-            <div class="btn-group">
-                <i class='icon bx bx-check-circle'></i>
-                <i class='icon bx bx-edit-alt'></i>
-                <i class='icon bx bx-trash'></i>
-            </div>
-        `;
-        
-        contenedorTareas.appendChild(nuevaTareaElemento);
-        document.getElementById('nueva-tarea').value = '';
-    } else {
-        alert('Por favor, ingresa una nueva tarea.');
-    }
+            nuevaTareaElemento.innerHTML = `
+                <p>${nuevaTareaTexto}</p>
+                <div class="btn-group">
+                    <i id='check' class='icon bx bx-check-circle'></i>
+                    <i id='edit' class='icon bx bx-edit-alt'></i>
+                    <i id='delete' class='icon bx bx-trash'></i>
+                </div>
+            `;
 
-});
+            contenedorTareas.appendChild(nuevaTareaElemento);
+            inputNuevaTarea.value = '';
+        } else {
+            alert('Por favor, ingresa una nueva tarea.');
+        }
+
+        const btnCheck = document.getElementById('check');
+        const contenCheck = document.querySelector('.tarea');
+        btnCheck.addEventListener('click', (e) => {
+                contenCheck.classList.toggle('checked');       
+        });
+
+    };
+
